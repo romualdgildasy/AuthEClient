9cfecb43792c9a1eadb6aeab3fa13866a9f77ac1
Romuald <romualdgilads6@gmail.com>
2025-10-13 14:31:51 +0100

    creation du user registration

---
+++ src/app/user/registration/registration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [],
+  templateUrl: './registration.component.html',
+  styles: ``
+})
+export class RegistrationComponent {
+
+}
