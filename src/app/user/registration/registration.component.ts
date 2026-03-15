f4a5f4acb40cd1e3e75484b0e9748b6bc66893ca
Romuald <romualdgilads6@gmail.com>
2026-03-15 01:47:14 +0100

    creation des pipes , modification du registartion ts,

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AbstractControl, FormBuilder, ReactiveFormsModule, ValidatorFn, Validators} from '@angular/forms';
+import { FirstkeyPipe } from '../../shared/pipes/firstkey.pipe';
+import { AuthService } from '../../shared/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule,CommonModule,FirstkeyPipe ],
   templateUrl: './registration.component.html',
   styles: ``
 })
@@ -14,32 +17,51 @@ export class RegistrationComponent {
     isSubmitted = false;
 
     //METHODE DE VALIDATION POUR LA CONFIRMATION DE MOT DE PASSE
-   passwordMatchValidator : ValidatorFn = (control:AbstractControl):null => {
+   passwordMatchValidator : ValidatorFn = (control: AbstractControl): null => {
     const password = control.get('password')
     const confirmPassword = control.get('confirmPassword')
 
-    if (password && confirmPassword && password.value !== confirmPassword.value) 
+    if (password && confirmPassword && password.value != confirmPassword.value) 
       confirmPassword.setErrors({ passwordMismatch: true });
      else 
       confirmPassword?.setErrors(null);
-      return null;
-  }
 
-    constructor(public formBuilder: FormBuilder) {
-        this.form = this.formBuilder.group({
-            fullName: ['', Validators.required],
-            email: ['', Validators.required,Validators.email],
-            password: ['', Validators.required, 
-              Validators.minLength(6),
-              Validators.pattern(/(?=.*[a-zA-z0-9])/)],
-            confirmPassword: ['',]
-        });
-        { validators: this.passwordMatchValidator }
+      return null;
     }
+
+    constructor(
+  public formBuilder: FormBuilder,
+  private service : AuthService,
+  private toastr : ToastrService) {
+    this.form = this.formBuilder.group({
+        fullName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [
+          Validators.required, 
+          Validators.minLength(6),
+          // Correction du pattern : il manquait un point avant l'étoile
+          Validators.pattern(/(?=.*[^a-zA-Z0-9 ])/)]], 
+        confirmPassword: [''],
+    }, { validators: this.passwordMatchValidator }); 
+}
 
     onSubmit() {
       this.isSubmitted = true;
-      console.log(this.form.value);
+      if(this.form.valid){
+        this.service.createUser(this.form.value)
+        .subscribe({
+          next: (res:any) => {
+            if(res.succeeded){
+              this.form.reset();
+              this.isSubmitted = false;
+              this.toastr.success('New user  created !', 'Registration Succesful')
+            }
+            else 
+            console.log('response:', res);
+          },
+          error: err => console.log('error', err)
+        });
+      }
     }
 
     hasDisplayableError(controlName: string): Boolean {
